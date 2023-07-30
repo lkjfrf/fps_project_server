e7d5d48431593c14c3951478b3eae142e6347b39
lkjfrf <34626154+lkjfrf@users.noreply.github.com>
2023-07-30 15:49:49 +0900

    송우현 main 수정

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"FPSProject/content"
 	"FPSProject/network"
 )
 
 func main() {
 	server := &network.Server{}
-
+	content.ContentManagerInit()
 	server.RunTCP(":1998")
 }
